9d97f0cb4e98478f099364c68abe1439738bc1a0
Lizeth <lizeth.giraldov@udea.edu.co>
2025-08-11 14:37:34 -0700

    It shows a bear

---
+++ bear.py
@@ -0,0 +1,13 @@
+"""
+Module for printing ASCII art showing a cat.
+"""
+
+def show_bear():
+    bear = r"""
+ʕ•ᴥ•ʔ
+    """
+    print("Here's a bear for you!")
+    print(bear)
+
+# Call the function
+show_bear()
