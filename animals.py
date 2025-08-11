2f1469cac6cddea8a74bb3c80c7fa925fe82843f
sjara <sjara@uoregon.edu>
2025-08-11 12:55:58 -0700

    Added module to show a cat.

---
+++ animals.py
@@ -0,0 +1,9 @@
+"""
+Script to print ASCII art showing a cat.
+"""
+
+import cat
+
+print('Here is a cat:')
+cat.print_cat()
+
---
+++ cat.py
@@ -0,0 +1,11 @@
+"""
+Module for printing ASCII art showing a cat.
+"""
+
+def print_cat():
+    cat_art = r"""
+ /\_/\  
+( o.o )
+ > ^ <
+    """
+    print(cat_art)
